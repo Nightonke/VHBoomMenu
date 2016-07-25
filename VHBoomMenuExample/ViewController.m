c2524d5df081afdd1f799285dab050a8f5d04064
Nightonke <Nightonke@outlook.com>
2016-07-25 14:42:05 +0800

    Initial Commit

---
+++ VHBoomMenuExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VHBoomMenuExample
+//
+//  Created by 黄伟平 on 16/7/25.
+//  Copyright © 2016年 黄伟平. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
